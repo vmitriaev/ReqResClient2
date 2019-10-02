6fee40c39c0c2bd15d36fd637b45e3b438f535fa
v.mitryaev <vmitriaev@gmail.com>
2019-10-02 23:34:58 +0300

    --

--- src/constructors.py
+++ src/constructors.py
@@ -1,5 +1,12 @@
-def error(message):
+def error_constructor(message):
     '''Вывод сообщения об ошибке'''
-    errMsg = ('ERROR: ' + message)
-    lines = '-' * len(errMsg)
-    return '\n' + lines + '\n' + errMsg + '\n' + lines + '\n'
+    errorMessage = ('ERROR: ' + message)
+    lines = '-' * len(errorMessage)
+    return '\n' + lines + '\n' + errorMessage + '\n' + lines + '\n'
+
+def counter(count):
+    '''Запоминает количество'''
+    return count
+
+r = counter(input('Введи число, а я запомню! Число: '))
+print('Ага! Твое число: ' + str(r))
