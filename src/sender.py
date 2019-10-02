5ba3b1fa978cdc5cac11d50218b70e83ff5bd33c
vmitriaev <vmitriaev@gmail.com>
2019-10-02 17:00:06 +0300

    --

--- src/sender.py
+++ src/sender.py
@@ -2,13 +2,3 @@ import requests, json
 
 with open('config.json') as config_file:
     conf = json.load(config_file)
-
-services_list = conf['services']
-
-default_url = conf['default_url']
-
-a = input('К какому сервису запрос?' + '\n' + str(services_list) + '\n')
-
-e = default_url + '/' + a.lower()
-
-print(e)
