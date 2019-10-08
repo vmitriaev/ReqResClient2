c1a994f88e1fc66e2d81ecd51f478fc4bd4b28d6
vmitriaev <vmitriaev@gmail.com>
2019-10-08 19:20:16 +0300

    --

--- src/constructors.py
+++ src/constructors.py
@@ -10,7 +10,7 @@ def error_constructor(message):
 
 def request_constructor(method, endpoint, body):
     if method == 'get':
-        request = requests.get(url=endpoint, data=body)
+        request = requests.get(url=endpoint)
         return request
     elif method == 'post':
         request = requests.post(url=endpoint, data=body)
@@ -22,12 +22,27 @@ def request_constructor(method, endpoint, body):
         request = requests.patch(url=endpoint, data=body)
         return request
     elif method == 'delete':
-        request = requests.delete(url=endpoint, data=body)
+        request = requests.delete(url=endpoint)
         return request
     else:
-        return error_constructor('WRONG METHOD')
+        return error_constructor('UNKNOWN METHOD')
 
 
 def response_json_constructor(request_data):
     response_data = json.dumps(request_data.json(), sort_keys=True, indent=2)
-    return response_data
+    return 'Response:\n' + response_data
+
+
+def response_code_handler(response_code):
+    if response_code == 200:
+        return '\nStatus: OK\nCode: 200\n'
+    elif response_code == 201:
+        return '\nStatus: CREATED\nCode: 201\n'
+    elif response_code == 204:
+        return '\nStatus: DELETED\nCode: 204\nResponse: NO CONTENT\n'
+    elif response_code == 400:
+        return '\nStatus: BAD REQUEST\nCode: 400\n'
+    elif response_code == 404:
+        return '\nStatus: NOT FOUND\nCode: 404\n'
+    else:
+        return error_constructor('unknown status code: ' + str(response_code))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,40 @@
-from src.constructors import request_constructor, response_json_constructor, error_constructor
+from src.constructors import request_constructor, response_json_constructor, error_constructor, response_code_handler
 
-request_params = ['get', 'https://reqres.in/api/users', None]
-
-print(response_json_constructor(request_constructor(request_params[0], request_params[1], request_params[2])))
+while True:
+    basic_url = 'https://reqres.in/api/'
+    print('For exit type STOP.' + '\n')
+    request_method = input('Type method: ')
+    if request_method.lower() in ('stop', 'break', 'exit'):
+        break
+    elif request_method.lower() == 'get':
+        request_url = input('Type url: ' + basic_url)
+        if request_url.lower == 'stop':
+            break
+        else:
+            request_params = [request_method, basic_url + request_url, None]
+            request_data = request_constructor(request_params[0], request_params[1], request_params[2])
+            response_json = response_json_constructor(request_data)
+            response_code = response_code_handler(request_data.status_code)
+            print(response_code + response_json)
+    elif request_method.lower() in ('post', 'put', 'patch'):
+        request_url = input('Type url: ' + basic_url)
+        if request_url.lower() == 'stop':
+            break
+        else:
+            request_body = input('Your JSON: ')
+            request_params = [request_method, basic_url + request_url, request_body]
+            request_data = request_constructor(request_params[0], request_params[1], request_params[2])
+            response_json = response_json_constructor(request_data)
+            response_code = response_code_handler(request_data.status_code)
+            print(response_code + response_json)
+    elif request_method.lower() == 'delete':
+        request_url = input('Type url: ' + basic_url)
+        if request_url.lower == 'stop':
+            break
+        else:
+            request_params = [request_method, basic_url + request_url, None]
+            request_data = request_constructor(request_params[0], request_params[1], request_params[2])
+            response_code = response_code_handler(request_data.status_code)
+            print(response_code)
+    else:
+        print(error_constructor('unknown method ' + request_method.upper()))
