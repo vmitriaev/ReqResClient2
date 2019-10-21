bb368b35bf1bb80ab71fc4d37d0d4a27e2276621
vmitriaev <vmitriaev@gmail.com>
2019-10-21 20:13:06 +0300

    --

--- src/temp.py
+++ src/temp.py
@@ -1,13 +1,10 @@
 import requests
 
-url = 'https://reqres.in/api/user/2'
+url = 'https://reqres.in/api/users'
 params = {
-    'MS_WHC': {
-        'CgPN': '79152268007',
-        'CdPN': '79299016973',
-        'Type': 0
-    }
+    "name": "morpheus",
+    "job": "leader"
 }
 
-response = requests.get(url, json=params)
+response = requests.post(url, json=params)
 print(response.status_code, response.text)
