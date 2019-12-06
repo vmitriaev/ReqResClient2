73f6dd07c441a753dadecb8f956edd2a37e7c0b2
vmitriaev <vmitriaev@gmail.com>
2019-12-06 19:02:04 +0300

    --

--- src/simple_request.py
+++ src/simple_request.py
@@ -1,11 +1,16 @@
-import requests
+import requests, json
 
-url = 'https://reqres.in/api/users'
-params = {
-    "name": "morpheus",
-    "job": "leader"
-}
 
-response = requests.post(url, json=params)
+def send_post_request():
+    url = 'https://reqres.in/api/users'
+    params = {
+        "name": "morpheus",
+        "job": "leader"
+    }
+    post_response_data = requests.post(url, json=params)
+    response_status_code = post_response_data.status_code
+    response_body = json.dumps(post_response_data.json(), sort_keys=True, indent=2)
+    return ('Status: ' + str(response_status_code) + '\n' + response_body)
 
-print(response.status_code, response.text)
+
+print(send_post_request())
